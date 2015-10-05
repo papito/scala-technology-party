712d55cf14369aa020bd619e180951f310327de1
papito <Andrei Taranchenko drey10@gmail.com>
2015-10-04 20:12:24 -0400

    Adding fake trello cards to list.

--- src/main/webapp/js/app/index.js
+++ src/main/webapp/js/app/index.js
@@ -3,13 +3,17 @@ IndexViewModel = BaseViewModel.extend({
     "use strict";
     this.base();
     console.log('Initializing index view model');
+
+    this.totalTrelloCards = ko.observable(1);
   },
 
   /*
    "TRELLO"
    */
 
-  addNewCard: function() {
-    console.log('Adding new card');
+  addNewCard: function(listId) {
+    var cardNo = this.totalTrelloCards();
+    $('#' + listId).append('<div id="card' + cardNo + '"class="card well">A card with some text #' + cardNo + '</div>');
+    this.totalTrelloCards(cardNo + 1);
   }
 });
